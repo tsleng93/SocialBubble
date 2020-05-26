eb16d27789cda90b4277b09da991d151f2b10e56
tsleng93 <51326999+tsleng93@users.noreply.github.com>
2020-05-26 20:31:37 +0100

    Update HouseholdMakerAge.m

--- HouseholdMakerAge.m
+++ HouseholdMakerAge.m
@@ -1,4 +1,4 @@
-function [M, B, C, Age] = HouseholdMakerAge(NumHouse, House_List, ProbHouse, House_Sizes, SizeBubble)
+function [M, B, C, Age, BH] = HouseholdMakerAge(NumHouse, House_List, ProbHouse, House_Sizes, SizeBubble,SizeBubble2)
 
 %Make Households H with probability ProbHouse
 %Also makes Bubble B
@@ -10,28 +10,31 @@ function [M, B, C, Age] = HouseholdMakerAge(NumHouse, House_List, ProbHouse, Hou
 
 if nargin == 0
     NumHouse =  10000;
-    House_List = load('House_List.csv');
-    ProbHouse = load('uk_composition_dist.csv');
-    House_Sizes = load('House_Sizes.csv');
+    House_List = load('Census_House_List.csv');
+    ProbHouse = load('Census_composition_dist.csv');
+    House_Sizes = load('Census_House_Sizes.csv');
     SizeBubble = 2;
+    SizeBubble2 = 2*SizeBubble;
 end
 
 %Always make NumHouse/SizeBubble an integer
-
-
 if mod(NumHouse, SizeBubble) ~= 0
     NumHouse = NumHouse + (SizeBubble - mod(NumHouse, SizeBubble));
 end
-
+if mod(NumHouse/2, SizeBubble2) ~= 0
+    NumHouse = NumHouse + (SizeBubble2 - mod(NumHouse/2, SizeBubble2))*2;
+end
 
 M = [];
 B = [];
+BH = [];
 ProbHouse = cumsum(ProbHouse);
 
 %Store Age vector
 Age = [];
 
 Bubbleadd = zeros(1,SizeBubble);
+Bubbleadd2 = zeros(1,SizeBubble2);
 %%%This makes bubbles of SizeBubble households
 for i  = 1:NumHouse
     r = rand;
@@ -43,6 +46,12 @@ for i  = 1:NumHouse
             M = blkdiag(M,sparse(ones(House_Sizes(j))));
             Bubbleadd(mod(i,SizeBubble)+1) = House_Sizes(j); 
             
+            if i > NumHouse/2
+                
+                Bubbleadd2(mod(i,SizeBubble2)+1) = House_Sizes(j);
+            end
+            
+            
             Age = [Age,House];
                 
             break;
@@ -51,13 +60,24 @@ for i  = 1:NumHouse
     
     if mod(i, SizeBubble) == 0
         B = blkdiag(B, sparse(ones(sum(Bubbleadd))));
+        
+        
+        if i > NumHouse/2
+             if mod(i, SizeBubble2) == 0
+                BH= blkdiag(BH, sparse(ones(sum(Bubbleadd2))));
+             end
+        else
+            BH= blkdiag(BH, sparse(ones(sum(Bubbleadd))));
+        end
     end
+    
+    
 end
 
 
 
-
 B = B - M;
+BH = BH - M;
 C = sum(M);
 
 %spy(M);
