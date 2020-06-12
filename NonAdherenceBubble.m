1d5258018be6eb649a8ca7ffeadbd41ed347c915
tsleng93 <51326999+tsleng93@users.noreply.github.com>
2020-06-12 14:20:57 +0100

    Update NonAdherenceBubble.m

--- NonAdherenceBubble.m
+++ NonAdherenceBubble.m
@@ -16,44 +16,54 @@ function B = NonAdherenceBubble(H, TypeorSize, Position, d)
 %Output
 %   - B is the adjacency matrix for bubble connections.
 
+%Authors: Trystan Leng and Connor White
+%Last update 12/06/2020.
+
+
+%Find all houses that satisfy the type or size
 temp = find(TypeorSize == 1);
 
+%randomly choose the households that will form bubbles
 r = randperm(length(temp), round(length(temp)*d));
 
+%initialise Bubble adjacency matrix
 B = sparse(zeros(length(H)));
 
+%L is number of households
 L = length(TypeorSize);
+%LH is size of population
 LH = length(H);
 
 for i = 1:(length(r)/2)
     
     j = 2*(i-1)+1;
     
+    %to deal with an odd number of entries
     if j+1 > length(r)
         break;
     else
         j2 = j+1;
     end
     
+    %choose households to join up into a bubble
     k = temp(r(j));
     k2 = temp(r(j2));
     
-    %k
-    
+    %Vector of individuals from first household entering bubble  
     if k == L
         House_to_Connect = Position(k):LH;
     else
         House_to_Connect = Position(k):(Position(k+1) - 1);
-    end
-    
-    
-    
+    end    
+    %Vector of individuals from second household entering bubble
     if k2 == L
         Newpartners = Position(k2):LH;
     else
         Newpartners = Position(k2):(Position(k2+1) - 1);
     end
-        
+   
+    %Join households together into the bubble by setting their
+    %entries in B to equal 1
     B(House_to_Connect, Newpartners) = 1;
     B(Newpartners, House_to_Connect) = 1;    
     
