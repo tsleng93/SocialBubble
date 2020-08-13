5445674906ab52619e37080c5c0958c3c7f79416
tsleng93 <51326999+tsleng93@users.noreply.github.com>
2020-08-13 11:47:28 +0100

    Update description

--- ScenarioTypeHouseBubble2.m
+++ ScenarioTypeHouseBubble2.m
@@ -1,7 +1,22 @@
 function [B,Inbubble] = ScenarioTypeHouseBubble2(H, TypeorSize, Position, Inbubble, d)
-%Connects that house to any other house
 
-%d = 1;
+%This function forms and adjacency matrix for Bubble connections.
+%The function requires a vector 'TypeorSize' which designates some attribute
+%of the household, e.g. if it is Size 1, if it has includes a child <10 years
+% old, etc.
+%ScenarioTypeHouseBubble 2 connects household with the attribute to any other household
+
+%Input:
+%   - H is the adjacency matrix for Household connections.
+%   - TypeorSize is a vector that contains either the type of house (e.g if
+%   it contains children or not)  or the size of each house.
+%   - position is a vector that stores the position in the population of 
+%   the first member of the household.
+%   - Inbubble is a vector storing whether a household is already part of a bubble
+%   - d is the proportion of eligible households who enter into a bubble
+
+%Output
+%   - B is the adjacency matrix for bubble connections.
 
 
 temp = find(TypeorSize == 1);
@@ -11,16 +26,6 @@ B = sparse(zeros(length(H)));
 L = length(TypeorSize);
 LH = length(H);
 
-
-%{
-for i = 1:length(Position)
-   if sum(Borig(Position(i),:)) > 0
-       Inbubble(i) = 1;
-   else
-       Inbubble(i) = 0;
-   end
-end
-%}
 
 
 for i = 1:d*(length(temp))
